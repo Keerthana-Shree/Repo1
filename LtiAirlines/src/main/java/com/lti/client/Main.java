72ec3a10cfba2afaaa5086dba9ab1096c4e3443c
Keerthana <shree.keerthu8@gmail.com>
2021-09-15 10:46:59 +0530

    This is my First commit

---
+++ LtiAirlines/src/main/java/com/lti/client/Main.java
@@ -0,0 +1,52 @@
+//package com.lti.client;
+//
+//public class main {
+//	public static void main(String[] args)
+//	{
+//		//Flight f=new Flight(1001,"mumbai","pune");
+//		//System.out.println(f.getFlightId());
+//		
+//		int num1=10;int num2=30;
+//		if (add(num1,num2)==30) {
+//			System.out.println("test passed");
+//		}
+//		else {
+//			System.out.println("test failed");
+//		}
+//	}
+//	
+//	public static int add(int n1,int n2) {
+//		return n1+n2;
+//	}
+//}
+
+package com.lti.client;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Flight f = new Flight(1001,"mumbai","pune");
+//		System.out.println(f.getFlightId());
+		
+		int num1=10;int num2=20;
+		if(add(num1,num2)==30) {
+			System.out.println("testpassed");
+		
+		}
+		else {
+			System.out.println("test failed");
+		}
+	}
+		
+	public static int add(int n1,int n2) {
+	 return n1+n2;	
+	}
+	public static boolean compare(int n1,int n2) {
+		if(n1==n2) {
+			return true;
+		}
+		return false;
+	}
+
+}
